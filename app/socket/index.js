faf25d335c9bee305bef39fd71b8b8b647393737
olegb <oleg.baslak@gmail.com>
2017-11-22 07:14:06 +0300

    Add users online status and showing list

--- app/socket/index.js
+++ app/socket/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var config = require('../config');
+var async = require('async');
 
 /**
  * Encapsulates all events supported by socket for listening and emiting
@@ -8,29 +9,52 @@ var config = require('../config');
  */
 var ioEvents = function (io) {
 
+    var sockets = [];
+
     let newMessageEvent = 'new message';
     let messageToClientsEvent = 'new message to clients';
     let newUserEvent = 'new user';
     let connectionEvent = 'connect';
     let disconnectEvent = 'disconnect';
     let identifyEvent = 'identify';
+    let rosterEvent = 'roster';
 
     io.on(connectionEvent, function (socket) {
         var ID = "U" + (socket.id).toString().substr(0, 5);
         io.emit(newUserEvent, ID)
         console.log('user ' + ID + ' connected');
 
+        sockets.push(socket.nickname);
+
         socket.on(newMessageEvent, function (msg) {
-            console.log("new message from " + ID + ': ' + msg);
-            io.emit(messageToClientsEvent, ID, msg);
+            console.log("new message from id " + ID + ' (user: ' + socket.nickname + '): ' + msg);
+            var text = String(msg || '');
+            io.emit(messageToClientsEvent, socket.nickname, text);
         });
+
 
         socket.on(disconnectEvent, function () {
             console.log('user disconnected');
             io.emit(disconnectEvent, ID)
         });
+
+        socket.on(identifyEvent, function (name) {
+            socket.nickname = name;
+            updateRoster();
+        });
+
+        function updateRoster() {
+            var connectedUsers = Object.keys(io.sockets.connected).map(function (socketId) {
+                return io.sockets.connected[socketId].nickname;
+            });
+            console.log("connected users:")
+            console.log(connectedUsers);
+
+            io.emit(rosterEvent, connectedUsers);
+        }
     });
 };
+
 
 /**
  * Initializes Socket.io
--- public/js/client.js
+++ public/js/client.js
@@ -2,29 +2,60 @@ window.onload = function () {
 
     var socket = io();
 
+    var $usersList = $('#users');
+    var $messagesList = $('#messages');
+
     let newMessageEvent = 'new message';
     let messageToClientsEvent = 'new message to clients';
     let newUserEvent = 'new user';
     let connectionEvent = 'connect';
     let disconnectEvent = 'disconnect';
+    let identifyEvent = 'identify';
+    let rosterEvent = 'roster';
 
     function printMessage(text) {
         console.log("appending new li");
-        $('#messages').append($('<li>').text(text));
+        $messagesList.append($('<li>').text(text));
         window.scrollTo(0, document.body.scrollHeight);
     }
 
+    function setName() {
+        var name = String($('#nickname').val() || "Anonymous");
+        console.log("new name is " + name)
+        socket.emit(identifyEvent, name)
+    }
+
+    function printUsers(users) {
+        console.log(document.getElementById("users"));
+        document.getElementById("users").innerHTML = "";
+
+        users.forEach(function (data) {
+            console.log("appending new user li");
+            $usersList.append($('<li>').text(data));
+        })
+    }
+
     $('form').submit(function () {
-        var messageText = $('#m').val().trim();
-        if (messageText === ' '){
+        var messageText = $('#message').val().trim();
+        if (messageText === ' ') {
             messageText = '';
         }
         console.log("message input value: " + messageText);
         if (messageText !== '') {
             socket.emit(newMessageEvent, messageText);
         }
-        $('#m').val('');
+        $('#message').val('');
         return false;
+    });
+
+    $('#nickname').on('input', function () {
+        setName();
+    });
+
+    socket.on('connect', setName);
+
+    socket.on(rosterEvent, function (names) {
+        printUsers(names);
     });
 
     socket.on(newUserEvent, function (name) {
